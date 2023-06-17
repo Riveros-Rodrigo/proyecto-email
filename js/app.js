9ee9471d8e784219ae8bfa8633b70599e4a8a4cb
Rodrigo Riveros <riverosdev@gmail.com>
2023-06-16 22:26:57 -0300

    subiendo funcion

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     // Seleccionar los elementos de la interfaz
-const inputEmail =  document.querySelector ("#email");
-const inputAsunto = document.querySelector ("#asunto");
-const inputMensaje = document.querySelector ("#mensaje");
+const inputEmail =  document.querySelector("#email");
+const inputAsunto = document.querySelector("#asunto");
+const inputMensaje = document.querySelector("#mensaje");
+const formulario = document.querySelector("#formulario")
 
 // Asignar eventos
 inputEmail.addEventListener("blur" , validar);
@@ -15,10 +16,26 @@ inputMensaje.addEventListener("blur" , validar);
 
 function validar(e) {
     if(e.target.value.trim() === ""){
-        console.log("Esta vacio");
+        mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement); //llamo a los id del html mediante target y me visualiza cada texto correspondiente
     } else{
-        console.log("Si hay algo");
+        console.log("Si hay algo...");
     }
+}
+
+function mostrarAlerta(mensaje, referencia){
+    //Comprueba si ya existe una alerta
+    const alerta = referencia.querySelector(".bg-red-600");
+    if(alerta){
+        alerta.remove();
+    }
+
+    //Generar alerta en el HTML
+    const error = document.createElement("P");
+    error.textContent = mensaje;
+    error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+
+    // Inyectar el error al formulario
+    referencia.appendChild(error);
 }
 
 });
