5fd00138ed71559426891334f544893695fb3af2
Rodrigo Riveros <riverosdev@gmail.com>
2023-06-17 01:05:47 -0300

    finalizo proyecto email

--- js/app.js
+++ js/app.js
@@ -1,41 +1,128 @@
 // esto es para que se ejecute todo el HTML una ves sea cargado
 document.addEventListener("DOMContentLoaded", function(){
 
+    const email = {
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
     // Seleccionar los elementos de la interfaz
 const inputEmail =  document.querySelector("#email");
 const inputAsunto = document.querySelector("#asunto");
 const inputMensaje = document.querySelector("#mensaje");
-const formulario = document.querySelector("#formulario")
+const formulario = document.querySelector("#formulario");
+const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+const btnReset = document.querySelector('#formulario button[type="reset"]');
+const spinner = document.querySelector('#spinner')
 
 // Asignar eventos
-inputEmail.addEventListener("blur" , validar);
+inputEmail.addEventListener("input" , validar);
 
-inputAsunto.addEventListener("blur" , validar);
+inputAsunto.addEventListener("input" , validar);
 
-inputMensaje.addEventListener("blur" , validar);
+inputMensaje.addEventListener("input" , validar);
+
+formulario.addEventListener('submit', enviarEmail)
+
+btnReset.addEventListener('click', function(e){
+    e.preventDefault(); //previene la accion del reset del form
+    resetFormulario();
+})
+
+function enviarEmail(e){
+    e.preventDefault();
+
+    spinner.classList.add('flex')
+    spinner.classList.remove('hidden')
+
+    setTimeout(() => {
+        spinner.classList.remove('flex')
+        spinner.classList.add('hidden')
+
+        //reiniciar el objeto
+        resetFormulario();
+
+        //Crear una alerta
+        const alertaExito = document.createElement('P');
+        alertaExito.classList.add('bg-green-500', 'text-white', 'p-2', 'text-center', 'rounded-lg', 'mt-10', 'font-bold', 'text-sm', 'uppercase');
+        alertaExito.textContent = 'Mensaje enviado correctamente';
+
+        formulario.appendChild(alertaExito);
+
+        setTimeout(() => {
+            alertaExito.remove()
+        },3000);
+
+    }, 3000) //para que oculte el spinner luego de 3seg
+}
 
 function validar(e) {
     if(e.target.value.trim() === ""){
         mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement); //llamo a los id del html mediante target y me visualiza cada texto correspondiente
-    } else{
-        console.log("Si hay algo...");
-    }
+        email[e.target.name] = '';
+        comprobarEmail();
+        return;
+    } 
+
+    if(e.target.id === 'email' && !validarEmail(e.target.value)){
+        mostrarAlerta('El email no es valido', e.target.parentElement);
+        comprobarEmail();
+        email[e.target.name] = '';
+        return;
+    };
+
+    limpiarAlerta(e.target.parentElement);
+
+    //Asignar los valores
+    email[e.target.name] = e.target.value.trim().toLowerCase(); // paso a minuscula con toLowerCase
+    //Comprobar el objeto de email
+    comprobarEmail();
 }
 
 function mostrarAlerta(mensaje, referencia){
+    limpiarAlerta(referencia); //reutilizo funcion para limpiar alerta
+    
+    //Generar alerta en el HTML
+    const error = document.createElement("P");
+    error.textContent = mensaje;
+    error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+    
+    // Inyectar el error al formulario
+    referencia.appendChild(error);
+}
+
+function limpiarAlerta(referencia){
     //Comprueba si ya existe una alerta
     const alerta = referencia.querySelector(".bg-red-600");
     if(alerta){
         alerta.remove();
     }
+}
 
-    //Generar alerta en el HTML
-    const error = document.createElement("P");
-    error.textContent = mensaje;
-    error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+function validarEmail(email){
+    const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+    const resultado = regex.test(email)
+    return resultado;
+}
 
-    // Inyectar el error al formulario
-    referencia.appendChild(error);
+function comprobarEmail(){
+    if(Object.values(email).includes('')){
+        btnSubmit.classList.add('opacity-50')
+        btnSubmit.disabled= true;
+        return
+    } 
+    btnSubmit.classList.remove('opacity-50')
+    btnSubmit.disabled= false;
+}
+
+function resetFormulario(){
+    //reiniciar el objeto
+    email.email='';
+    email.asunto='';
+    email.mensaje='';
+    formulario.reset();
+    comprobarEmail();
 }
 
 });
